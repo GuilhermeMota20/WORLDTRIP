86cb54a3863775ea92e5bd997528f2aa250fdcd6
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2022-08-24 18:47:25 -0300

    [CONFIG] Base do projeto

---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+    colors: {
+        highlight: {
+            '100': '#FFBA08',
+            '50': '#ffba0880'
+        },
+        textColor: {
+            '100': '#47585B',
+            '50': '#F5F8FA'
+        },
+        info: {
+            '100': '#999999',
+            '50': '#DADADA'
+        }
+    },
+    fonts: {
+        heading: 'Poppins',
+        body: 'Poppins'
+    },
+    styles: {
+        global: {
+            body: {
+                bg: 'textColor.50',
+                color: 'textColor.100'
+            }
+        }
+    }
+})
